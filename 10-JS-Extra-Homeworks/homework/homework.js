b6d291a03890e050075615210720bd2705a4296b
larry <larryjoseguarenas@gmail.com>
2022-10-05 12:05:10 -0300

    TareaExtra

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -1,5 +1,6 @@
 // No cambies los nombres de las funciones.
 
+
 function deObjetoAmatriz(objeto){
   // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
   // un par clave-valor en forma de matriz.
@@ -10,6 +11,10 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var result = Object.keys(objeto).map(function (key){
+    return [key,objeto[key]];
+  })
+  return result;
 }
 
 
@@ -18,6 +23,21 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  let obj = {};
+   
+  for (let i = 0; i < string.length; i++) {
+     
+    if (obj[string[i]] === undefined) {
+       
+      obj[string[i]] = 1;
+     } 
+     else {
+       
+      obj[string[i]] = obj[string[i]] + 1;
+     }
+   
+    }
+   return obj;
 }
 
 
@@ -26,7 +46,24 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+
+  var mayuscula = '';
+    
+  var minuscula = '';
+    
+    for (i = 0; i < s.length; i++){ 
+       
+      if(s[i] === s[i].toUpperCase()){ 
+            mayuscula = mayuscula + s[i]; 
+         }
+     
+      else {
+            minuscula = minuscula + s[i];
+          } 
+     }       
+    return (mayuscula + minuscula);
 }
+
 
 
 function asAmirror(str) {
@@ -35,21 +72,37 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+
+  let strProv = str.split('').reverse().join('');
+ 
+  return strProv.split(' ').reverse().join(' ');
 } 
+
 
 
 function capicua(numero){
   //Escribe una función, la cual recibe un número y determina si es o no capicúa.
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
-  //Escribe tu código aquí
-}
+  //Escribe tu código aqui
+  
+let array = numero.toString().split("").reverse().join("");
 
+  numero = numero.toString();
+
+  if (numero === array){
+    return "Es capicua";
+  }
+return "No es capicua";
+}
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  
+  return cadena.replace(/a/g,'').replace(/b/g,'').replace(/c/g,'');
+
 }
 
 
@@ -57,16 +110,31 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-}
 
+  return arr.sort((arr1, arr2) => arr1.length - arr2.length);
+}
 
 function buscoInterseccion(arreglo1, arreglo2){
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí 
+  var intersección = [];
+  
+  for (var i = 0; i < arreglo1.length; i++) {
+    
+    for (var j = 0 ; j < arreglo2.length; j++) {
+  
+      if (arreglo1[i] === arreglo2[j]) {
+  
+        intersección.push(arreglo1[i])
+      }
+  }
+}     return intersección;
+
 }
+
 
 
 
@@ -83,4 +151,5 @@ module.exports = {
    sortArray,
    buscoInterseccion,
 };
+
 
